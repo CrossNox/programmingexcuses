a66a59a0b7b5b07e10e823c65afdba8af774eccb
nox <ijmermet@gmail.com>
2019-01-08 23:21:20 -0300

    Added docstring for get_strategy based on intent signature

--- obliquestrategies/obliquestrategies.py
+++ obliquestrategies/obliquestrategies.py
@@ -3,7 +3,22 @@ import os.path
 
 editions = {1:'Edition_1_1975.py',2:'Edition_2_1978.py',3:'Edition_3_1979.py',4:'Edition_4_1996.py'}
 
-def get_strategy():
+def get_strategy(edition='any'):
+    """
+    Returns a random strategy
+
+    Args:
+        edition: the edition that you wish to use. Can be either of:
+            - 'any': a strategy from any edition will be returned
+            - (1975 | 1): either way refers to the first edition
+            - (1978 | 2): either way refers to the second edition
+            - (1979 | 3): either way refers to the second edition
+            - (1996 | 4): either way refers to the fourth edition
+            Default value: 'any'
+    Returns:
+        A string with a uniformly chosen strategy
+    """
+    if edition not in ['any',]
     edition_number = randint(1,4)
     module = os.path.abspath(os.path.dirname(__file__))
     edition = os.path.join(module, editions[edition_number])
